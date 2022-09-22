4203b5c3dbfbb9d4eaeac62e2b2975cb02fe2195
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 17:36:36 +0200

    redirect 7

--- redirect.js
+++ redirect.js
@@ -1,3 +1,9 @@
-const path = window.location.pathname;
-
-window.location.replace(`https://spirkomatus.com${path}`);
+if (window.sessionStorage.path) {
+  let path = window.sessionStorage.path;
+  window.history.pushState(null, null, path);
+  window.sessionStorage.removeItem("path");
+} else {
+  let path = window.location.pathname;
+  window.sessionStorage.path = path;
+  window.location.href = "/";
+}
