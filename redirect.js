f02aa0574b7126926e0c76af29b32ef816d73d35
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 16:59:43 +0200

    redirect

--- redirect.js
+++ redirect.js
@@ -1,15 +1,9 @@
-// const path = window.location.pathname;
-
-// window.location.replace(`https://spirkomatus.com${path}`);
-
-// if it's on index.html
 if (window.sessionStorage.path) {
-  let path = window.sessionStorage.path; // gets the path saved on sessionStorage
-  window.history.pushState(null, null, path); // push it on url
-  window.sessionStorage.removeItem("path"); // removes from session
+  let path = window.sessionStorage.path;
+  window.history.pushState(null, null, path);
+  window.sessionStorage.removeItem("path");
 } else {
-  // if it's on 404.html (path doens't exist)
-  let path = window.location.pathname; // get the path url
-  window.sessionStorage.path = path; // saves on sessionStorage
-  window.location.href = "/"; // go to index
+  let path = window.location.pathname;
+  window.sessionStorage.path = path;
+  window.location.href = "/";
 }
