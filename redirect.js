3c340a69e5ffca01d9e8507abcdeeb134f05c750
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 17:38:04 +0200

    redirect 8

--- redirect.js
+++
@@ -1,9 +0,0 @@
-if (window.sessionStorage.path) {
-  let path = window.sessionStorage.path;
-  window.history.pushState(null, null, path);
-  window.sessionStorage.removeItem("path");
-} else {
-  let path = window.location.pathname;
-  window.sessionStorage.path = path;
-  window.location.href = "/";
-}
