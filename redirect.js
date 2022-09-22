7b8994724217965770d17e263b96eb2747381c0e
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 13:20:57 +0200

    redirect and keep everything after / the same correct path

--- redirect.js
+++ redirect.js
@@ -1,3 +1,5 @@
-const path = window.location.pathname;
+// const path = window.location.pathname;
 
-window.location.replace(`https://spirkomatus.com${path}`);
+// window.location.replace(`https://spirkomatus.com${path}`);
+
+window.location.href = window.location.href.replace("coachofpeople.com", "spirkomatus.com");
