0709a10a9f9a86078e269272d84793d1644aa0c7
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 08:49:26 +0200

    redirect and keep everything after / the same correct path

--- redirect.js
+++ redirect.js
@@ -1,6 +1,3 @@
 const path = window.location.pathname;
 
-const newUrl = `https://spirkomatus.com${path}`;
-console.log(newUrl);
-
-window.location.replace(newUrl);
+window.location.replace(`https://spirkomatus.com${path}`);
