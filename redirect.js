e83d4f60fdc29746ab7f101d82de815b69524fe3
Martin Javorsky <martinjavorsky@sudolabs.io>
2022-09-22 16:51:31 +0200

    redirect

--- redirect.js
+++ redirect.js
@@ -1,5 +1,3 @@
-// const path = window.location.pathname;
+const path = window.location.pathname;
 
-// window.location.replace(`https://spirkomatus.com${path}`);
-
-// window.location.href = window.location.href.replace("coachofpeople.com", "spirkomatus.com");
+window.location.replace(`https://spirkomatus.com${path}`);
